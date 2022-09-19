0ed8ccf721b40aeb78df87650f60250ee9ffc2fc
Polina Verkhusha <polyaverh@gmail.com>
2022-09-19 19:26:37 -0400

    Commit!

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,62 @@
-console.log('hello world');
+console.log('Welcome to my website!');
+
+// const input = document.getElementById("name-input")
+// console.log(document.getElementById("name-input"))
+
+
+// const updateName = () => {
+//     console.log ("inside the function")
+// }
+
+// document.getElementById("name-input")
+// console.log(window)
+
+let count = 0;
+console.log(count)
+// count++
+// let btn = document.getElementById("btn");
+
+const change = function () {
+//    const userCount = count+1
+    count=count+1
+    console.log(count)
+    // document.getElementById("text").innerHTML = "hallo " + count;
+    // const name = document.getElementById("name-input").value;
+    // document.getElementById("text").innerHTML = "Times "+ name + " had clicked the button : "+ count;
+    document.getElementById("text").innerHTML =  count;
+    // window.alert(`You clicked the button ${userCount} times`)
+    //  let count: number
+    // if count==1 {
+    //     console.log("time")
+    // }else{
+    //     console.log("times")
+    // }
+}
+
+
+
+// change()
+// console.log(count)
+// console.log(userCount)
+
+
+
+// btn.click = function () {
+// btn.onclick = function () {
+ function updateCount() {
+    count++;
+    // const UserClicks = counter.innerText
+    // window.alert(${UserClicks})
+    document.getElementById("text").innerHTML = "hallo " + count;
+    // document.getElementById("btn") = counter.window;
+    // document.getElementById("btn").window=count
+    // document.getElementById("btn").counter.innerText = count
+    // console.log(document.getElementById("btn").window.alert = count)
+    // console.log(btn.click)
+    // window.alert(${btn.value})
+    //     disp.innerHTML = count;
+    // 
+}
+
+// btn.click()
+
